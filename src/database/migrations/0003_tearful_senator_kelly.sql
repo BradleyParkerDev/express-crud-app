44766f7723b777aa097fe317c33386f36cff0ce2
BradleyParkerDev <bradleyscottparker@outlook.com>
2025-02-02 11:33:09 -0600

    updated schema

---
+++ src/database/migrations/0003_tearful_senator_kelly.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "session_cron_jobs" RENAME COLUMN "id" TO "cron_job_id";
--- src/database/schemas/SessionCronJobs.ts
+++ src/database/schemas/SessionCronJobs.ts
@@ -1,7 +1,7 @@
 import { pgTable, varchar, uuid, timestamp, integer } from 'drizzle-orm/pg-core';
 
 const SessionCronJobs = pgTable('session_cron_jobs', {
-    id: uuid('id').primaryKey().defaultRandom(), // Unique identifier for each cron job entry
+    cronJobId: uuid('cron_job_id').primaryKey().defaultRandom(), // Unique identifier for each cron job entry
     lastChecked: timestamp('last_checked').notNull(),        // Time when the job was run
     sessionsDeleted: integer('sessions_deleted').notNull(), // Number of sessions deleted
 });
